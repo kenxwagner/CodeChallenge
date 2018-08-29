942308a2fe87657301bc16ce356f47ad72de3058
Ken Wagner <ken_xwagner@hotmail.com>
2018-08-28 23:32:14 -0700

    changes
    various changes and tests

---
+++ FileReader/kenFileReader/kenFileReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace kenFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int counter = 0;
+            string line;
+
+            StreamReader file =
+    new StreamReader(@"C:\Users\Ken\Documents\GitHub\CodeChallenge\FileReader\kenFileReader\filesToRead\TestFile.csv");
+            while ((line = file.ReadLine()) != null)
+            {
+                
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            Console.WriteLine("There were {0} lines.", counter);
+              
+            Console.ReadLine();
+        }
+    }
+}
+//@"C:\Users\Ken\Documents\GitHub\CodeChallenge\FileReader\kenFileReader\filesToRead\TestFile.csv"

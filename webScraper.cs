a97ded97a826a0eeb7a6f3a98d03d8448c2b4b0d
Ken Wagner <kenwagner@hotmail.com>
2020-02-17 17:36:22 -0800

    Update

--- webScraper.cs
+++ webScraper.cs
@@ -1,3 +1,6 @@
 private string Title { get; set; }
-    private string Url { get; set; }
-    private string siteUrl = "https://www.collabera.com";
+private string Url { get; set; }
+private string siteUrl = "https://www.collabera.com";
+
+public string[] QueryTerms { get; } = {123, 3456};
+
